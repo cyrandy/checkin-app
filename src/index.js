2e59d9b911cb8c066960a3820d5c8790ef6b1c48
cyrandy <cyrandy@gmail.com>
2015-11-29 16:18:49 +0800

    init project
    - es6 syntax
    - react available
    - webpack config
    - project build/development script

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  render() {
+    return <div>hello world</div>
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('content')
+)
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var webpack = require('webpack')
+
+module.exports = {
+  resolve: {
+    modulesDirectories: ['shared', 'node_modules'],
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, exclude:[/node_modules/], loader: 'babel?presets[]=react,presets[]=es2015' },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
+      { test: /\.sass$/, loader: 'style!css?sourceMap!sass?sourceMap&indentedSyntax&outputStyle=expanded' }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({ React: 'react' })
+  ]
+}
